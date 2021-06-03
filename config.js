a8ee927ed434b210b250d39b8e6846dc74b398d5
khoinguyen890 <85274403+khoinguyen890@users.noreply.github.com>
2021-06-03 12:42:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'hey quốc anh à',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim panda lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng quốc anh đã yêu panda ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Thế quốc anh đã liếm phèn panda chưa',
+    btnYes: 'ngày nào cũng liếm,từ 3 tấn giờ còn 10kg à',
+    btnNo: 'Không, bao giờ :))',
+    question:'Trên thế giới hơn 7 tỉ người mà sao quốc anh lại liếm phèn panda',
+    btnReply: 'Gửi cho panda',
+    reply: 'bởi vì thích',
+    mess: 'em biết mà 🥰.em là panda yêu quốc anh nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhe anh quốc anh.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
